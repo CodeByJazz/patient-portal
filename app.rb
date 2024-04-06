585ea7383a8174b19d33e2d1324bfdd757a2a2b7
Jasmynn Whitfield <jwhitfield.dev@gmail.com>
2024-04-06 17:14:31 -0400

    Create dashboard page for each pet

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require "redcarpet"
 require "fileutils"
 require "yaml"
 require "bcrypt"
+require "date"
 
 configure do 
   enable :sessions
@@ -15,19 +16,23 @@ end
 
 before do 
   @users = YAML.load_file(credentials_path)
-  # pattern = File.join(data_path, "*")
-  # @files = Dir.glob(pattern).map do |path|
-  #   File.basename(path)
-  # end
 end
 
-# def data_path 
-#   if ENV["RACK_ENV"] == "test"
-#     File.expand_path("../test/data", __FILE__)
-#   else
-#     File.expand_path("../data", __FILE__)
-#   end
-# end
+helpers do 
+  def sort_by_date(visits)
+    visits.sort_by do |visit|
+      Date.parse(visit)
+    end.reverse
+  end
+end
+
+def data_path 
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
 
 def credentials_path
   if ENV["RACK_ENV"] == "test"
@@ -50,11 +55,69 @@ end
 
 def error_for_sign_in(username)
   if !@users.key?(username)
-    "There is no account associated with this username. Please create an account below."
+    "There is no account associated with #{username}. Please create an account below."
   else
     "Invalid Credentials. Please Try Again."
   end
 end
+
+def invalid_name?(name)
+  name.empty?
+end
+
+def invalid_birthday?(birthday)
+  birthday.empty?
+end
+
+def invalid_type?(type)
+  type.empty?
+end
+
+def invalid_breed?(breed)
+  breed.empty?
+end
+
+def create_directory(name)
+  @file_path = File.join(data_path, name.downcase)
+  FileUtils.mkdir_p(@file_path)
+  FileUtils.mkdir_p(File.join(@file_path, "visits")) 
+  FileUtils.touch(File.join(@file_path, "vaccines.yml")) 
+  FileUtils.touch(File.join(@file_path, "prescriptions.yml")) 
+end
+
+def delete_directory(name)
+  FileUtils.rm_r("#{data_path}/#{name}")
+end
+
+def delete_pet(name) 
+  delete_directory(name)
+  @users[session[:username]]["pets"].delete(name)
+  File.open(credentials_path, 'w') { |f| YAML.dump(@users, f) }
+end
+
+def add_pet(name, birthday, type, breed, weight, microchip)
+  create_directory(name)
+
+  @users[session[:username]]["pets"][name.downcase] = {
+    "birthday" => birthday,
+    "type" => type,
+    "breed" => breed,
+    "weight" => weight,
+    "microchip" => microchip
+  }
+  File.open(credentials_path, 'w') { |f| YAML.dump(@users, f) }
+end
+
+def birthday_today?(birthday, today) 
+  today.month == birthday.month && today.day == birthday.day
+end
+
+def age(birthday, today) 
+  today.year - birthday.year - ((today.month > birthday.month || (today.month == birthday.month && today.day >= birthday.day)) ? 0 : 1)  
+end
+# not_found do 
+#   redirect "/"
+# end
 
 get "/" do 
   if session[:username] 
@@ -91,14 +154,111 @@ post "/users/signout" do
   redirect "/"
 end
 
+post "/:pet_name/delete" do 
+  pet_name = params[:pet_name]
 
+  delete_pet(pet_name)
+  
+  session[:message] = "#{pet_name.capitalize} has been deleted."
+  redirect "/"
+end 
 
+get "/add_pet" do 
+  erb :new_pet
+end
 
+post "/add_pet" do 
+  @name = params[:name].strip
+  @birthday = params[:birthday]
+  @type = params[:type]
+  @breed = params[:breed]
+  @weight = params[:weight]
+  @microchip = params[:microchip]
 
+  if invalid_name?(@name)
+    session[:message] = "Please enter a valid name"
+    erb :new_pet
+  elsif invalid_birthday?(@birthday) 
+    session[:message] = "Please select a valid birthday."
+    erb :new_pet
+  elsif invalid_type?(@type) 
+    session[:message] = "Please select a valid type."
+    erb :new_pet
+  elsif invalid_breed?(@breed) 
+    session[:message] = "Please select a valid breed."
+    erb :new_pet
+  else
+    add_pet(@name, @birthday, @type, @breed, @weight, @microchip)
+    session[:message] = "#{@name} has been added"
+    redirect "/"
+  end
+end
 
+get "/:pet_name/edit" do 
+  @name = params[:pet_name]
+  session[:original_name] = @name
+  @info = @users[session[:username]]["pets"][@name.downcase]
 
+  if @info.nil?
+    redirect "/"
+  else
+    erb :edit_pet
+  end
+end
 
+post "/edit" do 
+  @name = params[:name].strip
+  @birthday = params[:birthday]
+  @type = params[:type]
+  @breed = params[:breed]
+  @weight = params[:weight]
+  @microchip = params[:microchip]
+  @info = @users[session[:username]]["pets"][@name.downcase]
 
+  if invalid_name?(@name)
+    session[:message] = "Please enter a valid name"
+    erb :edit_pet
+  elsif invalid_birthday?(@birthday) 
+    session[:message] = "Please select a valid birthday."
+    erb :edit_pet
+  elsif invalid_type?(@type) 
+    session[:message] = "Please select a valid type."
+    erb :edit_pet
+  elsif invalid_breed?(@breed) 
+    session[:message] = "Please select a valid breed."
+    erb :edit_pet
+  else
+    delete_pet(session[:original_name])
+    add_pet(@name, @birthday, @type, @breed, @weight, @microchip)
+
+    session[:message] = "#{@name.capitalize} has been updated."
+    redirect "/"
+  end
+end
+
+get "/:pet_name/dashboard" do 
+  @name = params[:pet_name]
+  @info = @users[session[:username]]["pets"][@name.downcase]
+  birthday = Date.parse(@info["birthday"])
+  today = Time.now.utc.to_date
+  @age = age(birthday, today)
+
+  if birthday_today?(birthday, today) 
+    session[:message] = "Happy Birthday, #{@name.capitalize}!"
+  end
+
+  erb :dashboard
+end
+
+get "/:pet_name/visit_history" do
+  @name = params[:pet_name]
+  directory_path = File.join(data_path, "#{@name}/visits/*")
+  @visits = Dir.glob(directory_path).map do |path|
+    File.basename(path.gsub("_","-"))
+  end
+
+  erb :visit_history
+end
 
 
 
@@ -168,10 +328,8 @@ end
   -The homepage should include a list of all registered users 
     -Doctors will later be able to filter users by doctor to make for easy viewing
   -Doctors should be able to add new prescriptions, edit vaccine tracker, and add visit date and notes 
-    
-=end
 
-=begin
+
 
 ------------------CLIENT SIDE------------------
 Pet owners can login and view any relevant information about their pets
@@ -248,7 +406,6 @@ Pet owners can login and view any relevant information about their pets
     -Assign the return value to session[:message]
     -Return a status code of 422 (invalid data)
     -Render the `sign_in` view template again
-
   -Create a method called `error_for_sign_in` with one parameter: username
     -If the username does not exist in @users
       -Assign the session[:message] to: "There is no account associated with this username. Please create an account."
@@ -269,9 +426,10 @@ Pet owners can login and view any relevant information about their pets
   -Redirect user to "/sign_in" 
   -Display a message that says "Your account has been created. You may now sign in."
 
+
 ---ADD PET PAGE---
-  -When a user clicks the `Add New Patient` link:
-    -They should be redirected to "/:client_name/add_patient" page that contains:
+  -When a user clicks the `Add New Pet` link:
+    -They should be redirected to "/:client_name/add_pet" page that contains:
       -A title that says `Patient Information`
       -A form that asks the user for their pet's:
         -Name: 
@@ -280,9 +438,50 @@ Pet owners can login and view any relevant information about their pets
         -Breed:
         -Weight (if known):
         -Microchip # (if known):
-      -A button that says `Add Patient` (patient should appear on homepage)
-  -Redirect user to "/" 
+      -A button that says `Add Patient`
+  -Redirect user to "/" (patient should now appear on homepage)
   -Display message that says "#pet_name has been added.""
+
+  --------IMPLEMENTATION FOR ADD PET PAGE---------
+  -Create a view template called new_pet 
+    -Add a form that asks for the pet's: 
+      -Name: 
+      -Birthday: 
+        -Input type should be "date"
+          -(use birthday to calculate age in the dashboard)
+      -Type: 
+       -Input should be a dropdown menu with different selections (Canine, Feline, Snake, Rabbit, Hamster)
+      -Breed:
+       -Input should be a dropdown menu with different selections (Yorkie, Chiahuahu, Corgi, Husky, Poodle)
+      -Weight:
+        -Set the default parameter to be ""
+      -Microchip:
+        -Set the default parameter to be ""
+    -Add a button that says "Add Pet" and submits the form
+  -Create a post route for "/add_pet"
+    -Assign the parameters to corresponding variables 
+      -Validate the name of the pet 
+        -Create a method called valid_name? with one parameter: name
+          -Check to see if the given name is NOT an empty string
+      -Validate the microchip #
+        -Create a method called valid_microchip?
+          -Check to see if the microchip number is all digits
+            -Convert the string to an integer to a string
+            -If the converted string is equal to the original string 
+              -if the string length is 9, 10, or 15 digits return true 
+            -Otherwise return false
+    -If valid_name? && valid_microchip is true
+      -Add the pet to the pets hash for the current user 
+        -Create a key in the pets hash with the given name 
+        -Assign the value to a hash that includes all the pet's info
+        -Add this new key-value pair back into the YAML file 
+      -Set the session[:message] to "#{pet_name}" has been added."
+      -Redirect the user "/"
+    -Otherwise 
+      -set the session message to "Please enter a valid microchip number."
+      -set the session message to "Please enter a valid name."
+
+
 
 ---EDIT PET INFO PAGE---
   -When a user clicks the `edit` link:
@@ -300,9 +499,32 @@ Pet owners can login and view any relevant information about their pets
   -Redirect user to "/" 
   -Display message that says "#pet_name's profile has been updated."
 
+--------IMPLEMENTATION FOR ADD PET PAGE---------
+
+-Create a get route that renders the edit_pet page 
+-Add a title that says `Edit Patient Information`
+-Create a form that asks the user for their pet's new:
+  -Name: 
+  -Birthday: 
+  -Type: 
+  -Breed:
+  -Weight(lbs):
+  -Microchip #:
+    -Add exisiting info associated with the specific pet on each part of the form
+      -In the get route, access the pet_name name provided by the url
+      -Access the hash for the specified pet in the users pets array 
+      -Use the appropriate values for each part of the form 
+-Add a button that says `Save Changes` to update patient info
+  -Create a post route that does the steps outlined for the add new pet page
+-Set the session[:message] to "#pet_name's profile has been updated."
+-Redirect the user home 
+-updated pet info should be displayed on the dashboard and homepage 
+
+    
+
 ---DASHBOARD---
   -When a user clicks the `Pet_Name` link:
-    -They should be redirected to "/:client_name/:patient_name/dashboard" page that contains:
+    -They should be redirected to "/:patient_name/dashboard" page that contains:
       -A title that says `Dashboard`
       -A table with the selected pet's:
         -Name
@@ -317,9 +539,40 @@ Pet owners can login and view any relevant information about their pets
         -Vaccine Tracker (table that shows when they were performed/ if they're due)
         -RX Prescriptions (how to use instructions, dosage, quantity, expiration etc.)
 
-=end
 
-=begin  
+--------IMPLEMENTATION FOR DASHBOARD PAGE---------
+-Create a get route for "/:patient_name/dashboard"
+-Create an erb template called dashboard
+  -Add a title called Dashboard 
+  -Create a table with a row for the pet's:
+    -Name
+    -Birthday (if it's their birthday a message should pop up that says "Happy Birthday, pet name!")
+      -If the current month and day are equal to the pet's birth month and day 
+        -Set the session message to be "Happy Birthday #{pet_name}!"
+    -Pet type (canine/feline/etc.)
+    -Breed
+    -Age (calculated by the birthday)
+      -Calculate the age of the pet 
+        -Require the date class in the application 
+        -Create a method called age with one parameter(dob)
+          -Get the current date 
+          -Subtract the current year from the dob year 
+          -Use the following code: 
+            require 'date'
+
+            def age(dob)
+              now = Time.now.utc.to_date
+              now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+            end
+    -Weight 
+    -Microchip #
+  -Add the following links to the bottom of the page:
+    -Visit history (w/ links for each visit to view doctors notes)
+    -Vaccine Tracker (table that shows when they were performed/ if they're due)
+    -RX Prescriptions (how to use instructions, dosage, quantity, expiration etc.)  
+
+
+
 ------------------FEATURES TO BE ADDED------------------
   -Generate a random number (1-10000) and assign it to be the users account #
     -Append it to the users hash in users.yml (MAYBE???)
@@ -332,6 +585,8 @@ Pet owners can login and view any relevant information about their pets
     -When they do, they'll be prompted to rate the medication based on its: effectiveness, changes in behavior, and any remaining concerns
     -Medical Documents (doctors can upload health certs, external records, etc)
   -Doctors will later be able to filter users by doctor to make for easy viewing
+  -When a certain pet type is selected, make the breed dropdown menu specific to that pet type 
+  -Ask for confirmation before deleting a pet from the patient portal
 
 ----DOCTOR SIDE----
 Doctors can login and view any relevant information about their clients (owners/pets)
@@ -352,8 +607,4 @@ Doctors can login and view any relevant information about their clients (owners/
     -Pet info
   -Doctors should be able to add new prescriptions for pets, edit vaccine tracker, and add visit date and notes 
   
-Passwords: 
-Jay = finalfantasy7
-Jazz = ilovepie!
-Jill = rosesarered
 =end
